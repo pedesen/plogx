e470e96702019b5e90de4bf789f6338edafbbeb5
pedesen <pedesen@googlemail.com>
2013-11-12 01:20:15 +0100

    add day_stats and month_stats in the frontend using flot graph

--- plogx/app.py
+++ plogx/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import render_template
 from flask.ext.pymongo import PyMongo
-from database import get_stats_per_day
+from database import get_stats_per_day, get_stats_per_month
 from bson.json_util import dumps
 from datetime import datetime
 
@@ -19,6 +19,13 @@ def stats_per_day(date):
     d = str(date)
     day = datetime(int(d[:4]), int(d[4:6]), int(d[6:]))
     log_items = get_stats_per_day(mongo.db, day)
+    return dumps(log_items)
+
+@app.route("/stats_per_month/<int:date>")
+def stats_per_month(date):
+    d = str(date)
+    month = datetime(int(d[:4]), int(d[4:6]), 1)
+    log_items = get_stats_per_month(mongo.db, month)
     return dumps(log_items)
 
 if __name__ == "__main__":
--- plogx/static/js/plogx.js
+++ plogx/static/js/plogx.js
@@ -1,5 +1,86 @@
+
+
 $(document).ready(function(){
-	$.getJSON( "/stats_per_day/20131106", function( data ) {
-		console.log(data)
-	});
+    var day_stats;
+    var generate_stats_per_month = function(data){
+        var chart_visits = new Array();
+        var chart_page_impr = new Array();
+
+        day_stats = data["day_stats"];
+        for (var i = 0; i < day_stats.length; i++) {
+            chart_visits.push([i+1, day_stats[i]["num_visits"]])
+            chart_page_impr.push([i+1, day_stats[i]["num_page_impressions"]])
+            console.log(new Date(day_stats[i]["day"]["$date"]));
+        }
+
+        chart_visits = new Array();
+        chart_page_impr = new Array();
+        for (var i = 0; i < 30; i++) {
+          page_impr = Math.floor((Math.random()*50)+20);
+          visits = page_impr - Math.floor((Math.random()*20)+1);
+          
+          chart_visits.push([i+1, visits]);
+          chart_page_impr.push([i+1, page_impr]);
+        }
+
+        var dataset = [{
+          label: "Page Impressions",
+          data: chart_page_impr,
+          lines: { show: false },
+          color: "#8db9cc",
+          bars: {
+            show: true,
+            barWidth: 0.8,
+            align: "center",
+            fill:1
+          }
+        },{
+          label: "Visitors",
+          data: chart_visits,
+          lines: { show: false },
+          color: "#14568a",
+          bars: {
+            show: true,
+            barWidth: 0.4,
+            align: "center",
+            fill:1
+          }
+        }]
+
+        console.log(dataset)
+        console.log(day_stats);
+
+        var plot = $.plot(month_stats, dataset, {
+          grid: {
+            clickable: true
+          }
+        });
+    }
+    $.getJSON( "/stats_per_month/201311", generate_stats_per_month);
+
+    $("#month_stats").bind("plotclick", function(event, pos, item){
+      if (item){
+        console.log(item);
+        day = new Date(day_stats[item["dataIndex"]]["day"]["$date"]);
+
+        $.getJSON("/stats_per_day/" + moment(day).format("YYYYMMDD"), function(data){
+          console.log(data);
+          var paths = "";
+          for (i=0; i< data["path_stats"].length; i++){
+            path_stats = data["path_stats"][i];
+            paths+=path_stats["num_visits"]+"x "+path_stats["path"]+"<br/>"
+          }
+
+
+          $("#day_stats").html("Stats for "+ moment(day).format("MMMM Do YYYY")+":<br/>"+
+            "Page Impressions: "+data["num_page_impressions"]+"<br/>"+
+            "Visitors: "+data["num_visits"]+"<br/><br/>"+
+            "Paths:<br/>"+paths
+          );
+
+        })
+        
+
+      }
+    })
 });
