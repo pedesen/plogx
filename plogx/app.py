9d0965b95e34ae041538c72523f8df97f2302912
pedesen <pedesen@googlemail.com>
2014-09-09 14:23:53 +0200

    add current month selection

--- plogx/app.py
+++ plogx/app.py
@@ -28,10 +28,15 @@ def raw_logs_per_day(date):
     log_items = database.get_raw_logs_per_day(mongo.db, day)
     return render_template("raw_logs_per_day.html", log_items=log_items)
 
+@app.route("/stats_per_month")
 @app.route("/stats_per_month/<int:date>")
-def stats_per_month(date):
-    d = str(date)
-    month = datetime(int(d[:4]), int(d[4:6]), 1)
+def stats_per_month(date=None):
+    if date is None:
+        now = datetime.now()
+        month = datetime(now.year, now.month, 1)
+    else:
+        d = str(date)
+        month = datetime(int(d[:4]), int(d[4:6]), 1)
     log_items = database.get_stats_per_month(mongo.db, month)
     return dumps(log_items)
 
